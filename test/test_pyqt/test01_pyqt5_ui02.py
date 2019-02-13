6a91f62f8f783ac255f5c161d5bc72efa79953fe
834441353 <834441353@qq.com>
2019-02-13 11:30:00 +0800

    a

--- test/test_pyqt/test01_pyqt5_ui02.py
+++ test/test_pyqt/test01_pyqt5_ui02.py
@@ -24,4 +24,10 @@ class Ui_childFrom(object):
     def retranslateUi(self, childFrom):
         _translate = QtCore.QCoreApplication.translate
         childFrom.setWindowTitle(_translate("childFrom", "Form"))
+        self.textEdit.setHtml(_translate("childFrom", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">aa+bb</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
 
